283dc6825ed00e1d8834a9c560f9c85c63f84b0c
Parvez Mosaraf <79097428+parvezmosaraf@users.noreply.github.com>
2023-02-10 04:06:58 +0600

    Add files via upload

---
+++ final_version.py
@@ -0,0 +1,64 @@
+import cv2
+import numpy as np
+import tkinter as tk
+from pynput.mouse import Button, Controller
+
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+mouse = Controller()
+
+cap = cv2.VideoCapture(0)
+
+# Define the clicking function
+def perform_click(x, y):
+    mouse.position = (x, y)
+    mouse.click(Button.left, 1)
+
+# GUI window to start and stop the program
+root = tk.Tk()
+root.title("Head Tracking")
+root.geometry("200x100")
+
+def start_tracking():
+    while True:
+        ret, frame = cap.read()
+        if ret == False:
+            print("Error reading video stream")
+            break
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 0, 255), 2)
+            roi_gray = gray[y:y+h, x:x+w]
+            roi_color = frame[y:y+h, x:x+w]
+
+            # Find the center of the face
+            center = (x + w//2, y + h//2)
+
+            # Move the mouse cursor to the center of the face
+            mouse.position = center
+
+            # Perform a left click at the center of the face
+            perform_click(*center)
+
+        cv2.imshow("Head Tracking", frame)
+
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+def stop_tracking():
+    cap.release()
+    cv2.destroyAllWindows()
+    root.quit()
+
+start_button = tk.Button(root, text="Start", command=start_tracking)
+stop_button = tk.Button(root, text="Stop", command=stop_tracking)
+
+start_button.pack(pady=10)
+stop_button.pack(pady=10)
+
+root.mainloop()
